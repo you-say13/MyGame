0d2066d7b0b9063bccc1254d2b3faa427692a26d
DESKTOP-FLV5PB4\YuseiOkawa <nukohitu@gmail.com>
2024-06-06 20:56:46 +0900

    命名を変更

--- src/pages/game_components/GameScreen.tsx
+++ src/pages/game_components/GameScreen.tsx
@@ -1,38 +1,38 @@
 import { useEffect } from "react";
 import { gsProps } from "./Interface";
 
-const GameScreen = (props:gsProps) => {
+const GameScreen = (props: gsProps) => {
 
-    const gs = document.getElementById("GameScreen");
+  const gameScreenElement = document.getElementById("GameScreen");
 
-    useEffect(() => {
-        let tmp = []
-        if(gs !== undefined && gs !== null){
-            for(let i = 0; i < gs.children.length; i++){
-                tmp.push(gs.children[i].id)
-            }
-            props.setEnemyId(tmp)
-        }
-    },[])
-    
-    return (
-        <div id="GameScreen" className="flex items-center justify-center w-10/12 h-2/3 bg-gray-900 text-white">
-            {
-                props.enemy.map((Element, index)=>{
-                    return(
-                        <div key={index} className="flex flex-col justify-center items-center">
-                            {
-                                index === props.cursor && props.phase === 2 &&
-                                <div>↓</div>
-                            }
+  useEffect(() => {
+    let temporaryList = []
+    if (gameScreenElement !== undefined && gameScreenElement !== null) {
+      for (let i = 0; i < gameScreenElement.children.length; i++) {
+        temporaryList.push(gameScreenElement.children[i].id)
+      }
+      props.setEnemyId(temporaryList)
+    }
+  }, [])
 
-                            <div key={index} id={index.toString()} className={`w-1/${100/props.enemy.length}`}><img className="h-1/2 w-1/2 flex mx-auto" src={`/images/${Element.src}`} /></div>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
+  return (
+    <div id="GameScreen" className="flex items-center justify-center w-10/12 h-2/3 bg-gray-900 text-white">
+      {
+        props.enemy.map((Element, index) => {
+          return (
+            <div key={index} className="flex flex-col justify-center items-center">
+              {
+                index === props.cursor && props.phase === 2 &&
+                <div>↓</div>
+              }
+
+              <div key={index} id={index.toString()} className={`w-1/${100 / props.enemy.length}`}><img className="h-1/2 w-1/2 flex mx-auto" src={`/images/${Element.src}`} /></div>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default GameScreen
